360f8f579f09bb5a9d3de3c64f8b7cbd0d6a0517
qinjo <60745301+qinjo@users.noreply.github.com>
2023-08-14 09:49:36 +0800

    Update notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/jojodudu/Notion-DoIt-05135ae5ae7c4172ade20315f21b7dce?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
